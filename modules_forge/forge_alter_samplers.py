3524b067b1685592a1dd3e80b41bdd35ecf7fce6
Brendan Lackey <brendan.lackey@outlook.com>
2025-12-17 04:53:39 -0700

    Add third-order step support and enhance sampler options in CFG++
    - Enhance AlterSampler to support arbitrary extra options
    - Add 3rd Order option to sd_samplers_common
    - Add following samplers:
      - DEIS
      - SA Solver (+exp variants)
      - Seeds 2
      - Seeds 3
      - gradient estimation (& CFG++)
      - Res Multistep A (& CFG++)
      - Res Multistep CFG++
      - Extended Reverse-Time SDE

--- modules_forge/forge_alter_samplers.py
+++ modules_forge/forge_alter_samplers.py
@@ -15,6 +15,7 @@ class AlterSampler(sd_samplers_kdiffusion.KDiffusionSampler):
 
         # Store additional kwargs like cfg_pp for later use
         self.sampler_kwargs = kwargs
+        self.is_cfg_pp = sampler_name.endswith("_cfg_pp") or kwargs.get("cfg_pp", False)
         super().__init__(sampler_function, sd_model)
 
     def initialize(self, p) -> dict:
@@ -29,12 +30,12 @@ class AlterSampler(sd_samplers_kdiffusion.KDiffusionSampler):
         return extra_params_kwargs
 
     def sample(self, p, *args, **kwargs):
-        if p.cfg_scale > 2.0:
+        if self.is_cfg_pp and p.cfg_scale > 2.0:
             logging.warning("CFG between 1.0 ~ 2.0 is recommended when using CFG++ samplers")
         return super().sample(p, *args, **kwargs)
 
     def sample_img2img(self, p, *args, **kwargs):
-        if p.cfg_scale > 2.0:
+        if self.is_cfg_pp and p.cfg_scale > 2.0:
             logging.warning("CFG between 1.0 ~ 2.0 is recommended when using CFG++ samplers")
         return super().sample_img2img(p, *args, **kwargs)
 
@@ -46,20 +47,25 @@ def build_constructor(sampler_key: str, **kwargs) -> Callable:
     return constructor
 
 
-def create_cfg_pp_sampler(sampler_name: str, sampler_key: str, **kwargs) -> "sd_samplers_common.SamplerData":
+def create_alter_sampler(sampler_name: str, sampler_key: str, copy_params_name: str | None = None, **kwargs) -> "sd_samplers_common.SamplerData":
     config = {}
-    base_name = sampler_name.removesuffix(" CFG++")
-    for name, _, _, params in sd_samplers_kdiffusion.samplers_k_diffusion:
-        if name == base_name:
-            config = params.copy()
-            break
+    if copy_params_name:
+        for name, _, _, params in sd_samplers_kdiffusion.samplers_k_diffusion:
+            if name == copy_params_name:
+                config = params.copy()
+                break
 
     return sd_samplers_common.SamplerData(sampler_name, build_constructor(sampler_key=sampler_key, **kwargs), [sampler_key], config)
 
 
 samplers_data_alter = [
-    create_cfg_pp_sampler("DPM++ 2M CFG++", "dpmpp_2m_cfg_pp"),
-    create_cfg_pp_sampler("Euler a CFG++", "euler_ancestral_cfg_pp"),
-    create_cfg_pp_sampler("Euler CFG++", "euler_cfg_pp"),
-    create_cfg_pp_sampler("Res Multistep CFG++", "res_multistep", cfg_pp=True),
+    create_alter_sampler("DPM++ 2M CFG++", "dpmpp_2m_cfg_pp", copy_params_name="DPM++ 2M"),
+    create_alter_sampler("Euler a CFG++", "euler_ancestral_cfg_pp", copy_params_name="Euler a"),
+    create_alter_sampler("Euler CFG++", "euler_cfg_pp", copy_params_name="Euler"),
+    create_alter_sampler("Res Multistep a", "res_multistep", eta=0.),
+    create_alter_sampler("Res Multistep CFG++", "res_multistep", eta=0., cfg_pp=True),
+    create_alter_sampler("Res Multistep a CFG++", "res_multistep", eta=1., cfg_pp=True),
+    create_alter_sampler("Gradient Estimation CFG++", "gradient_estimation", cfg_pp=True),
+    create_alter_sampler("Exponential Heun2 x0", "seeds_2", copy_params_name="Seeds2", s_noise=0., eta=0., solver_type="phi_2", r=1.),
+    create_alter_sampler("Exponential Heun2 x0 SDE", "seeds_2", copy_params_name="Seeds2", s_noise=1., eta=1., solver_type="phi_2", r=1.),
 ]
