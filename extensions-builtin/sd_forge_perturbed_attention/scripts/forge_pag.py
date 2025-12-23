47f5ef319e49f64a2822f8cc40cd7181db22d816
Brendan Lackey <brendan.lackey@outlook.com>
2025-12-23 12:36:30 -0700

    minor default tweaks

--- extensions-builtin/sd_forge_perturbed_attention/scripts/forge_pag.py
+++ extensions-builtin/sd_forge_perturbed_attention/scripts/forge_pag.py
@@ -10,11 +10,8 @@ from modules.ui_components import InputAccordion
 class PerturbedAttentionGuidanceForForge(scripts.Script):
     sorting_priority = 13
 
-    attenuated_scale = 3.0
-    doPAG = True
-
     def title(self):
-        return "PerturbedAttentionGuidance Integrated"
+        return "Perturbed Attention Guidance"
 
     def show(self, is_img2img):
         return scripts.AlwaysVisible
@@ -22,14 +19,20 @@ class PerturbedAttentionGuidanceForForge(scripts.Script):
     def ui(self, is_img2img):
         with InputAccordion(False, label=self.title()) as enabled:
             with gr.Row():
-                scale = gr.Slider(label='Scale', minimum=0.0, maximum=100.0, step=0.1, value=3.0)
+                scale = gr.Slider(label='Scale', minimum=0.0, maximum=100.0, step=0.1, value=2.5)
                 attenuation = gr.Slider(label='Attenuation (linear, % of scale)', minimum=0.0, maximum=100.0, step=0.1,
-                                        value=0.0)
+                                        value=15.0)
             with gr.Row():
                 start_step = gr.Slider(label='Start step', minimum=0.0, maximum=1.0, step=0.01, value=0.0)
-                end_step = gr.Slider(label='End step', minimum=0.0, maximum=1.0, step=0.01, value=1.0)
+                end_step = gr.Slider(label='End step', minimum=0.0, maximum=1.0, step=0.01, value=0.4)
             with gr.Row():
-                skip_hires = gr.Checkbox(label='Skip HiRes Fix', value=False, visible=not is_img2img)
+                apply_to = gr.Radio(
+                    label='Apply To',
+                    choices=['Both', 'Base Only', 'Hires Only'],
+                    value='Hires Only',
+                    visible=not is_img2img,
+                    info="Control when PAG is applied during generation"
+                )
 
         self.infotext_fields = [
             (enabled, lambda d: d.get("pagi_enabled", False)),
@@ -37,29 +40,37 @@ class PerturbedAttentionGuidanceForForge(scripts.Script):
             (attenuation, "pagi_attenuation"),
             (start_step, "pagi_start_step"),
             (end_step, "pagi_end_step"),
-            (skip_hires, "pagi_skip_hires"),
+            (apply_to, "pagi_apply_to"),
         ]
 
-        return enabled, scale, attenuation, start_step, end_step, skip_hires
-
-    def denoiser_callback(self, params):
-        thisStep = (params.sampling_step) / (params.total_sampling_steps - 1)
-
-        if thisStep >= PerturbedAttentionGuidanceForForge.PAG_start and thisStep <= PerturbedAttentionGuidanceForForge.PAG_end:
-            PerturbedAttentionGuidanceForForge.doPAG = True
-        else:
-            PerturbedAttentionGuidanceForForge.doPAG = False
+        return enabled, scale, attenuation, start_step, end_step, apply_to
 
     def process_before_every_sampling(self, p, *script_args, **kwargs):
-        enabled, scale, attenuation, start_step, end_step, skip_hires = script_args
+        enabled, scale, attenuation, start_step, end_step, apply_to = script_args
 
-        if not enabled or (getattr(p, 'is_hr_pass', False) and not skip_hires):
+        if not enabled:
+            return
+
+        # Check if we should skip based on apply_to setting
+        is_hires = getattr(p, 'is_hr_pass', False)
+        if apply_to == 'Base Only' and is_hires:
+            return
+        elif apply_to == 'Hires Only' and not is_hires:
             return
 
         PerturbedAttentionGuidanceForForge.scale = scale
-        PerturbedAttentionGuidanceForForge.PAG_start = start_step
-        PerturbedAttentionGuidanceForForge.PAG_end = end_step
-        on_cfg_denoiser(self.denoiser_callback)
+        PerturbedAttentionGuidanceForForge.start_step = start_step
+        PerturbedAttentionGuidanceForForge.end_step = end_step
+        PerturbedAttentionGuidanceForForge.do_pag = True
+
+        def denoiser_callback(params):
+            current_step = params.sampling_step / (params.total_sampling_steps - 1)
+            PerturbedAttentionGuidanceForForge.do_pag = (
+                current_step >= PerturbedAttentionGuidanceForForge.start_step and
+                current_step <= PerturbedAttentionGuidanceForForge.end_step
+            )
+
+        on_cfg_denoiser(denoiser_callback)
 
         unet = p.sd_model.forge_objects.unet.clone()
 
@@ -69,15 +80,16 @@ class PerturbedAttentionGuidanceForForge(scripts.Script):
         def post_cfg_function(args):
             denoised = args["denoised"]
 
-            if PerturbedAttentionGuidanceForForge.scale <= 0.0:
+            if PerturbedAttentionGuidanceForForge.scale <= 0.0 or not PerturbedAttentionGuidanceForForge.do_pag:
                 return denoised
 
-            if not PerturbedAttentionGuidanceForForge.doPAG:
-                return denoised
+            model = args["model"]
+            cond_denoised = args["cond_denoised"]
+            cond = args["cond"]
+            sigma = args["sigma"]
+            x = args["input"]
+            options = args["model_options"].copy()
 
-            model, cond_denoised, cond, sigma, x, options = \
-                args["model"], args["cond_denoised"], args["cond"], args["sigma"], args["input"], args[
-                    "model_options"].copy()
             new_options = set_model_options_patch_replace(options, attn_proc, "attn1", "middle", 0)
 
             degraded, _ = calc_cond_uncond_batch(model, cond, None, x, sigma, new_options)
@@ -97,10 +109,10 @@ class PerturbedAttentionGuidanceForForge(scripts.Script):
             pagi_attenuation=attenuation,
             pagi_start_step=start_step,
             pagi_end_step=end_step,
+            pagi_apply_to=apply_to,
         ))
 
         return
 
-    def postprocess(self, params, processed, *args):
+    def postprocess(self, p, processed, *args):
         remove_current_script_callbacks()
-        return
