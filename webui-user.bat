e67fdcf8c73f72976f69be2107939c461b059c23
Brendan Lackey <brendan.lackey@outlook.com>
2026-01-19 11:21:44 -0700

    match upstream more closely

--- webui-user.bat
+++ webui-user.bat
@@ -3,9 +3,8 @@
 :: set PYTHON=
 :: set GIT=
 :: set VENV_DIR=
-
-set COMMANDLINE_ARGS=
-
+set DEBUG_MEMORY=1
+set COMMANDLINE_ARGS=--adv-samplers --uv --skip-python-version-check --skip-torch-cuda-test --skip-version-check --skip-prepare-environment --skip-install --cuda-malloc --cuda-stream
 :: --xformers --sage --uv
 :: --pin-shared-memory --cuda-malloc --cuda-stream
 :: --skip-python-version-check --skip-torch-cuda-test --skip-version-check --skip-prepare-environment --skip-install
